a824e3132c453d5a49cdad591fdc048284cd4f07
jann88 <jannylynemiaz@gmail.com>
2023-07-24 18:26:33 -0700

    declaring variables to be utilized

--- printf_int.c
+++ printf_int.c
@@ -6,5 +6,13 @@
  */
 int printf_int(va_list args)
 {
+	/*takes variable no of arguments of type va_list*/
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
 
 }
