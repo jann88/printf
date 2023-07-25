653c1f5f5a17ad819b3d231658df8ec9b31cfdd5
jann88 <jannylynemiaz@gmail.com>
2023-07-24 18:39:00 -0700

    checks for highest exponents, stoores in last and updates the integer

--- printf_int.c
+++ printf_int.c
@@ -20,8 +20,24 @@ int printf_int(va_list args)
 		n = -n;
 		last = -last;
 		i++;
-
 	}
-
-
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+	return (i);
 }
