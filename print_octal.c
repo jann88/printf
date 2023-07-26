0e857ff860fe2a44bf5d1c61cf195837aa8758d9
codinBabe <teboola2@gmail.com>
2023-07-26 09:19:52 -0700

    fix error

--- print_octal.c
+++ print_octal.c
@@ -2,10 +2,9 @@
 /**
  * print_octal - a function that print octal
  * @o:integer to print
- * added flags to check for active flags
  * Return:zero on success
  */
-int print_octal(va_list o, Flags flags)
+int print_octal(va_list o)
 {
 	unsigned int num = va_arg(o, unsigned int);
 
